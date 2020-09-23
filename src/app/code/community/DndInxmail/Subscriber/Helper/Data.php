e81568884d7833ddf334cf662d9ca471bde37c1e
maurice <24431825+msifrt@users.noreply.github.com>
2020-09-23 15:03:14 +0200

    [INXMA2-6] Improve hash function
    - Fix 404 bug in Backend Sync

--- src/app/code/community/DndInxmail/Subscriber/Helper/Data.php
+++ src/app/code/community/DndInxmail/Subscriber/Helper/Data.php
@@ -50,7 +50,7 @@ class DndInxmail_Subscriber_Helper_Data extends DndInxmail_Subscriber_Helper_Abs
      */
     public function getHashKey()
     {
-        return (string)Mage::getConfig()->getNode('global/crypt/key');
+        return substr(hash('sha256', '7gWTgEHDp3cZsXAzj8UWWSwUyrM6jNZA'), 0, 32);
     }
 
     /**
